6ac904865019feaa2d1cfe1738ef777866912a3f
prashanthyempati <prashanth.yempati@gmail.com>
2026-07-14 20:48:55 +0530

    Delete ESP32-Cricket-Scoreboard-64x64-2 directory

--- ESP32-Cricket-Scoreboard-64x64-2/include/Display.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <MD_Parola.h>
-class DisplayManager{
-public:
- void begin();
- void showStartup();
-};
--- ESP32-Cricket-Scoreboard-64x64-2/src/main.cpp
+++
@@ -1,5 +0,0 @@
-#include <Arduino.h>
-#include "Display.h"
-DisplayManager display;
-void setup(){display.begin();display.showStartup();}
-void loop(){}
